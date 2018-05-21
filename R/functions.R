35e105b8cdf158b858d962d27c4c828e7d501187
Edward Roche <eroche@localhost.localdomain>
2018-05-21 11:57:53 +0100

    Code now mirrors the Steem API Calls with only useful functions visible to the user.

--- R/functions.R
+++ R/functions.R
@@ -1,102 +1,100 @@
-#' Loop Through Blog
+
+# steemR user Functions ----
+# These functions will be the main interface for users of the steemR package
+
+
+#' getBlog
 #'
-#' Loop throught all the posts of a users blog
+#' Get all main posts from a users blog (excluding Resteems)
 #'
-#' @param username Username of Blog Author
+#' @param username Username of Blog Author.
 #'
-#' @return Data Frame with Details of Blog Posts
+#' @return Data.Table with Details of Blog Posts
 #'
 #' @examples
-#' loopPosts()
+#' getBlog("eroche")
 #'
 #' @export
-loopPosts <- function(username){
+getBlog <- function(username){
+  ##First Call to Retrieve Blogs Limits 100 Posts
+  permlink <- ""
+  results <- get_discussions_by_author_before_date(tolower(username), permlink)
+  count <- length(results)
 
-
-  link = paste0(',"start_permlink":"',"",'"')
-  query <- paste0('{"jsonrpc":"2.0", "method":"tags_api.get_discussions_by_author_before_date", "params":{"author":"',tolower(username),'","limit":100', link ,'}, "id":1}')
-  results <- getPosts(query)
-  count <- nrow(results)
-
+  ##Start at last permlink of Blog and get the next 100 posts. Loop until less than 100 are retrieved
   while (count == 100) {
     ##Add in logic here to skip calls if not finding any more results
-    link = paste0(',"start_permlink":"',results[nrow(results), "permlink"],'"')
-    query <- paste0('{"jsonrpc":"2.0", "method":"tags_api.get_discussions_by_author_before_date", "params":{"author":"',tolower(username),'","limit":100', link ,'}, "id":1}')
-    tmpresults <- getPosts(query)
-    count <- nrow(tmpresults)
-    results <- rbind(results,tmpresults)
+    permlink <- results[[length(results)]]$permlink
+    tmpresults <- get_discussions_by_author_before_date(tolower(username), permlink)
+    count <- length(tmpresults)
+    results <- c(results,tmpresults)
   }
-  results <- results[!duplicated(results$permlink)]
-  results[,comments:=children]
-  results[,date:=as.Date(date)]
+
+  ##Convert the Raw Steem data to a cleaned data.table
+  results <- cleanData(results)
+
   return(results)
 }
 
-#' Format Number
-#'
-#' Format A Number as a Character with comma seperators
-#'
-#' @param x The numeric character
-#'
-#' @return character representation of number
-#'
-#' @examples
-#' None
-#'
-#' @export
+
+
+# Steem API Calls ----
+# These functions will be wrappers for the steem api calls
+
+#Get the Steem posts from a user profile using the Steem API calls
+
+get_discussions_by_author_before_date <- function(username, permlink){
+  link = paste0(',"start_permlink":"',permlink,'"')
+  query <- paste0('{"jsonrpc":"2.0", "method":"tags_api.get_discussions_by_author_before_date", "params":{"author":"',username,'","limit":100', link ,'}, "id":1}')
+
+  r <- POST("https://api.steemit.com", body = query)
+  data <- content(r, "parsed", "application/json")
+  discussions <- data$result$discussions
+
+
+
+  return(discussions)
+
+}
+
+
+# Get the latest steem account count from the Steem API calls
+get_account_count <- function(){
+  query <- '{"jsonrpc":"2.0", "method":"condenser_api.get_account_count", "params":[], "id":1}'
+  r <- httr::POST("https://api.steemit.com", body = query)
+  data <- httr::content(r, "parsed", "application/json")
+  paste("Number of Accounts : ", data$result)
+}
+
+#Get the latest number of follow count for the specified user using the Steem API calls
+
+get_follow_count <- function(user){
+  query <- paste0('{"jsonrpc":"2.0", "method":"follow_api.get_follow_count", "params":{"account":"', user ,'"}, "id":1}')
+  r <- httr::POST("https://api.steemit.com", body = query)
+  data <- httr::content(r, "parsed", "application/json")
+  print(paste("Number of Followers : ", data$result$follower_count))
+  (paste("Number Following : ", data$result$following_count))
+}
+
+
+# Utility Functions ----
+# These functions will process the data returned from the API calls and perform operations such as clensing ...
+
+#Format a number as a character with comma seperators
 formatNum <- function(x){
   prettyNum(format(x, digits=2, nsmall=2), scientific=FALSE, big.mark=",")
 }
 
 
-#' Create Hyperlink
-#'
-#' add a hyperlink to the data table
-#'
-#' @param author username of blog author
-#' @param link link to create
-#' @param title title visible name for link
-#'
-#' @return Data Frame with Details of Blog Posts
-#'
-#' @examples
-#' None
-#'
-#' @export
+#Format and add a hyperlink to the data table
+
 createLink <- function(author,link, title) {
   sprintf('<a href="https://steemit.com/@%s/%s" target="_blank">%s</a>',author, link, title)
 }
 
 
-#' Flatten List
-#'
-#' utility function to flatten a multi level list
-#'
-#' @param lst list to flatten
-#'
-#' @return flattened list
-#'
-#' @examples
-#' None
-#'
-#' @export
-flatten <- function(lst) {
-  do.call(c, lapply(lst, function(x) if(is.list(x)) flatten(x) else list(x)))
-}
+#utility convert the Steem Reputation to a 2 digit number. Ported Code to R to format rep in readable format https://github.com/steemit/steem-js/blob/master/src/formatter.js
 
-
-#' Steem Reputation Formatter
-#'
-#' utility convert the Steem Reputation to a 2 digit number. Ported Code to R to format rep in readable format https://github.com/steemit/steem-js/blob/master/src/formatter.js
-#'
-#' @param rep long form value
-#'
-#' @return 2 digit value
-#'
-#' @examples
-#' None
-#'
-#' @export
 reputation.formatter <- function(rep) {
   neg = grepl("-", rep)
   rep <- sub("-","",rep)
@@ -110,26 +108,12 @@ reputation.formatter <- function(rep) {
   return(out)
 }
 
-#' Get Steem Posts
-#'
-#' utility function to get the Steem posts from a user profile. This function is a wrapper to an RPC call.
-#'
-#' @param query get_discussions_by_author_before_date query
-#'
-#' @return list of blog posts
-#'
-#' @examples
-#' None
-#'
-#' @export
-getPosts <- function(query){
-  ##query <- paste0('{"jsonrpc":"2.0", "method":"tags_api.get_discussions_by_author_before_date", "params":{"author":"',tolower("haejin"),'","limit":10' ,'}, "id":1}')
+#utility to convert the Steem Data Dump to a nice Data.Frame with relevant fields for analysis
 
-  ##query <- paste0('{"jsonrpc":"2.0", "method":"tags_api.get_discussions_by_author_before_date", "params":{"author":"',tolower("eroche"),'","limit":10' ,'}, "id":1}')
+cleanData <- function(discussions){
 
-  r <- POST("https://api.steemit.com", body = query)
-  data <- content(r, "parsed", "application/json")
-  discussions <- data$result$discussions
+
+
   n <- length(discussions)
 
   results <- data.table(author=as.character(rep(NA,n)),
@@ -146,13 +130,11 @@ getPosts <- function(query){
                         created=as.character(rep(NA,n)),
                         body=as.character(rep(NA,n)),
                         json=as.character(rep(NA,n)),
-                        depth=as.numeric(rep(NA,n)),
                         votes=as.numeric(rep(NA,n)),
                         voter=as.character(rep(NA,n)),
                         voter.rshares=as.character(rep(NA,n)),
                         voter.time=as.character(rep(NA,n)),
                         voter.percent=as.character(rep(NA,n)),
-                        ##                      vote_details=as.list(rep(NA,n)),
                         children=as.numeric(rep(NA,n)),
                         pending_payout=as.numeric(rep(NA,n)),
                         paid_payout=as.numeric(rep(NA,n)),
@@ -161,21 +143,17 @@ getPosts <- function(query){
 
 
   for(i in 1 : n) {
-    discussion <- flatten(discussions[i])
+    discussion <- discussions[[i]]
     results[i,"author"] <- discussion$author
     results[i,"author_reputation"] <- discussion$author_reputation
     results[i,"permlink"] <- discussion$permlink
-
     results[i,"title"] <- discussion$title
-
     results[i,"created"] <- discussion$created
-
     results[i,"body"] <- discussion$body
-
     results[i,"json"] <- discussion$json_metadata
     results[i,"image_count"] <- length(fromJSON(discussion$json_metadata)$image)
-    skip=FALSE
 
+    skip=FALSE
     result = tryCatch({
       grepl("tags", results[i,"json"])
     }, error = function(e) {
@@ -207,74 +185,46 @@ getPosts <- function(query){
       }
     }
 
-    results[i,"depth"] <- discussion$depth
+##    results[i,"depth"] <- discussion$depth
     results[i,"children"] <- discussion$children
-    results[i,"votes"] <- sum(grepl("active_votes.voter", names(discussion)))
-    results[i,"voter"] <- paste(unname(do.call(rbind, discussion[grepl("active_votes.voter", names(discussion))])),collapse=" ")
-    results[i,"voter.rshares"] <- paste(unname(do.call(rbind, discussion[grepl("active_votes.rshares", names(discussion))])),collapse=" ")
-    results[i,"voter.time"] <- paste(unname(do.call(rbind, discussion[grepl("active_votes.time", names(discussion))])),collapse=" ")
-    results[i,"voter.percent"] <- paste(unname(do.call(rbind, discussion[grepl("active_votes.percent", names(discussion))])),collapse=" ")
+
+    votes <- data.table(do.call(rbind, discussion$active_votes))
+
+
+
+    results[i,"votes"] <- nrow(votes)
+    results[i,"voter"] <- paste(unlist(votes$voter),  collapse=' ')
+    results[i,"voter.rshares"] <- paste(unlist(votes$rshares),  collapse=' ')
+    results[i,"voter.time"] <- paste(unlist(votes$time),  collapse=' ')
+    results[i,"voter.percent"] <- paste(unlist(votes$percent),  collapse=' ')
 
     ##results[i, "vote_details"] <- do.call(rbind, (lapply(data$result$discussions[[i]]$active_votes, unlist)))
 
-    results[i,"pending_payout"] <- as.numeric(discussion$pending_payout_value1)/1000
+    results[i,"pending_payout"] <- as.numeric(discussion$pending_payout_value[1])/1000
 
-    results[i,"paid_payout"] <- as.numeric(discussion$total_payout_value1)/1000
+    results[i,"paid_payout"] <- as.numeric(discussion$total_payout_value[1])/1000
 
   }
 
   results[,total_payout:= paid_payout + pending_payout]
-  ##alias for total_payout
-  results[,payout:= total_payout]
 
-  results[,date:= strptime(results$created, format="%Y-%m-%dT%H:%M:%S")]
-
+  results[,datetime:= strptime(results$created, format="%Y-%m-%dT%H:%M:%S")]
+  results$created <- NULL
+  results[,date:=as.Date(datetime)]
   results[, year_month:=format(date, "%Y-%b")]
 
 
   results[,author_rep:= reputation.formatter(author_reputation)]
+  results$author_reputation <- NULL
+  results[,comments:=children]
+  results$children <- NULL
 
+
+  ##Remove Duplicate posts
+  results <- results[!duplicated(results$permlink)][order(-date)]
 
   return(results)
 
 }
 
 
-#' Steem Blockchain Stats
-#'
-#' Return Key Stats for the Steem Blockchain
-#'
-#' @param NULL
-#'
-#' @return xxx
-#'
-#' @examples
-#' None
-#'
-#' @export
-steemStats <- function(){
-  query <- '{"jsonrpc":"2.0", "method":"condenser_api.get_account_count", "params":[], "id":1}'
-  r <- httr::POST("https://api.steemit.com", body = query)
-  data <- httr::content(r, "parsed", "application/json")
-  paste("Number of Accounts : ", data$result)
-}
-
-#' getFollow
-#'
-#' Return data for the Selected User
-#'
-#' @param user
-#'
-#' @return blog
-#'
-#' @examples
-#' None
-#'
-#' @export
-getFollow <- function(user){
-  query <- paste0('{"jsonrpc":"2.0", "method":"follow_api.get_follow_count", "params":{"account":"', user ,'"}, "id":1}')
-  r <- httr::POST("https://api.steemit.com", body = query)
-  data <- httr::content(r, "parsed", "application/json")
-  print(paste("Number of Followers : ", data$result$follower_count))
-  (paste("Number Following : ", data$result$following_count))
-}
